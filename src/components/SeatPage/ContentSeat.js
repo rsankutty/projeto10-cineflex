53fc33d08da9e2b768b06e5de9001d13db9e3dac
rsankutty <r.sankutty@gmail.com>
2022-12-12 13:39:57 -0300

    post reserva de assento

--- src/components/SeatPage/ContentSeat.js
+++ src/components/SeatPage/ContentSeat.js
@@ -1,16 +1,51 @@
 import styled from "styled-components";
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom"
+
 
 export default function ContentSeat({ seats }) {
+  let name = ""
+  let cpf=""
+  const ids = []
+  const navigate = useNavigate()
 
+  function enviarDados(event) {
+    event.preventDefault(); // impede o redirecionamento
+    console.log(ids,name,cpf)
+  
+    const requisicao = axios.post("https://mock-api.driven.com.br/api/v8/cineflex/seats/book-many", {
+      ids: ids,
+      name: name,
+      cpf: cpf
+    })
+  
+    requisicao.then(() => navigate("/")) 
+  }
 
-  function SeatButton({ name, isAvailable }) {
+  function SeatButton({ id, name, isAvailable }) {
     const [statusColor, setStatusColor] = useState('#C3CFD9');
     const [borderColor, setBorderColor] = useState('#808F9D');
+    const [seatStatus, setSeatStatus] = useState('available');
 
     function chooseSeat() {
-      setStatusColor('#1AAE9E')
-      setBorderColor('#0E7D71')
+      if (isAvailable === false) {
+        alert("Esse assento não está disponível")
+      } else {
+        if (seatStatus === 'available') {
+          setSeatStatus('selected')
+          setStatusColor('#1AAE9E')
+          setBorderColor('#0E7D71')
+          ids.push(id)
+
+        } else if (seatStatus === 'selected') {
+          setSeatStatus('available')
+          setStatusColor('#C3CFD9')
+          setBorderColor('#808F9D')
+          ids.splice(ids.indexOf(id), 1)
+        }
+        console.log(ids)
+      }
     }
 
     return (
@@ -27,9 +62,10 @@ export default function ContentSeat({ seats }) {
       <Wrapper>
         <SeatsContainer>
           {seats.map((elem) => (
-            <SeatButton key={elem.id} name={elem.name} isAvailable={elem.isAvailable} />
+            <SeatButton key={elem.id} id={elem.id} name={elem.name} isAvailable={elem.isAvailable} />
           ))}
         </SeatsContainer>
+
         <Legenda>
           <ItemLegenda>
             <Seat borderColor={'#0E7D71'} color={'#1AAE9E'}></Seat>
@@ -44,14 +80,64 @@ export default function ContentSeat({ seats }) {
             <p>Indisponível</p>
           </ItemLegenda>
         </Legenda>
+
+        <InputContainer>
+          <form onSubmit={enviarDados} >
+            <label htmlFor="name">Nome do comprador:</label>
+            <input id="name" required type="text" placeholder="Digite seu nome"  onChange={e => name = e.target.value} >
+            </input>
+            <label htmlFor="cpf">CPF do comprador:</label>
+            <input id="cpf" required type="text" placeholder="Digite seu CPF" onChange={e => cpf = e.target.value} >
+            </input>
+            <ButtonContainer>
+              <button>Reservar assento(s)</button>
+            </ButtonContainer>
+          </form>
+        </InputContainer>
+
       </Wrapper>
     </Content>
   );
 }
 
+const ButtonContainer = styled.div`
+display: flex;
+margin-top: 60px;
+justify-content: center;
+align-items: center;
+button{
+  width: 225px;
+height: 42px;
+background-color: #E8833A;
+border-radius: 3px;
+color: white;
+border:none
+
+}
+`;
+
+
+const InputContainer = styled.div`
+width: 327px;
+font-size: 18px;
+input {
+  height: 51px;
+  border: 1px solid #D5D5D5;
+  border-radius: 3px;
+  width: 310px;
+  font-size: 18px;
+  padding-left: 18px;
+  margin-bottom: 10px;
+  margin-top: 5px;
+  ::placeholder{
+    font-style: italic;
+  }
+}
+`;
+
+
 const Content = styled.div`
   width: 100%;
-  height: 1000px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -108,6 +194,7 @@ const Legenda = styled.div`
   align-items: center;
   justify-content: space-around;
   /* background-color: green; */
+  margin-bottom: 41px;
 `;
 
 const ItemLegenda = styled.div`
